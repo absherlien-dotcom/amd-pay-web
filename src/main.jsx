82a00e5810ad0be41cc7ca42b1c108d28600f183
absherlien-dotcom <absherlien@gmail.com>
2026-05-19 00:09:10 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -737,7 +737,7 @@ const sendMessage = async (text) => {
       },
     ]);
   }
-}
+};
 
   const lastUserQuestion = [...messages].reverse().find((message) => message.from === "user")?.text || "مرحباً، أحتاج مساعدة بخصوص أمد باي";
 
@@ -836,7 +836,6 @@ const sendMessage = async (text) => {
       )}
     </>
   );
-}
 
 function App() {
   return (
